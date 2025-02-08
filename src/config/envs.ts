3218676f969675d0e83b79c5cb7c53dbc1ea6ab0
red <redmoart@gmail.com>
2025-02-07 20:00:22 -0500

    config:⚙️Add environment configuration and TypeScript setup

---
+++ src/config/envs.ts
@@ -0,0 +1,39 @@
+import "dotenv/config";
+import * as joi from "joi";
+
+interface EnvVars {
+  PORT: number;
+  DB_HOST: string;
+  DB_PORT: number;
+  DB_NAME: string;
+  DB_USERNAME: string;
+  DB_PASSWORD: string;
+}
+
+const envsSchema = joi
+  .object({
+    PORT: joi.number().required(),
+    DB_HOST: joi.string().required(),
+    DB_PORT: joi.number().required(),
+    DB_NAME: joi.string().required(),
+    DB_USERNAME: joi.string().required(),
+    DB_PASSWORD: joi.string().required(),
+  })
+  .unknown(true);
+
+const { error, value } = envsSchema.validate(process.env);
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+const envVars: EnvVars = value;
+
+export const envs = {
+  port: envVars.PORT,
+  dbHost: envVars.DB_HOST,
+  dbPort: envVars.DB_PORT,
+  dbName: envVars.DB_NAME,
+  dbUsername: envVars.DB_USERNAME,
+  dbPassword: envVars.DB_PASSWORD,
+};
